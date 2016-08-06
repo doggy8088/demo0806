ede2cbf86f1e0218bf74b75dee55052475ba7e25
Will <doggy.huang@gmail.com>
2016-08-06 10:14:34 +0800

    04 將網頁頁首的部分建立成 HeaderComponent 元件

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.css'],
+  directives: [HeaderComponent]
 })
 export class AppComponent {
   title = 'app works!';
